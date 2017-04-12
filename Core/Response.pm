41af7a78eeec5e33b067f618e441a262eb89ebaa
Gentilhomme <gentilhomme.thomas@gmail.com>
2017-04-12 15:01:55 +0200

    Update Response.pm

--- Core/Response.pm
+++ Core/Response.pm
@@ -10,7 +10,14 @@ sub new {
     my ($class,$argRef) = @_;
     my $this = {
         _rc => $argRef->{rc},
-        _data => $argRef->{data}
+        _data => $argRef->{data},
+        _time => $argRef->{time},
+        _callback => $argRef->{callback},
+        _retry => $argRef->{retry},
+        _addr => defined $argRef->{addr} ? $argRef->{addr} : "undefined",
+        _port => defined $argRef->{port} ? $argRef->{port} : "undefined",
+        _robot => defined $argRef->{robot} ? $argRef->{robot} : "undefined", 
+        _timeout => $argRef->{timeout}
     };
     return bless($this,ref($class) || $class);
 }
@@ -18,6 +25,57 @@ sub new {
 sub rc {
     my ($self) = @_;
     return $self->{_rc};
+}
+
+sub getCallback() {
+    my ($self) = @_;
+    return $self->{_callback};
+}
+
+sub getTime() {
+    my ($self) = @_;
+    return $self->{_time};
+}
+
+sub getRetry() {
+    my ($self) = @_;
+    return $self->{_retry};
+}
+
+sub getAddr() {
+    my ($self) = @_;
+    return $self->{_addr};
+}
+
+sub getPort() {
+    my ($self) = @_;
+    return $self->{_port};
+}
+
+sub getRobot() {
+    my ($self) = @_;
+    return $self->{_robot};
+}
+
+sub getTimeout() {
+    my ($self) = @_;
+    return $self->{_timeout};
+}
+
+sub dump {
+    my ($self) = @_;
+    my @Dump = (
+        rc          => $self->rc(),
+        time        => $self->getTime(),
+        callback    => $self->getCallback(),
+        time        => $self->getTime(),
+        retry       => $self->getRetry(),
+        addr        => $self->getAddr(),
+        port        => $self->getPort(),
+        robot       => $self->getRobot(),
+        timeout     => $self->getTimeout()
+    );
+    return @Dump;
 }
 
 sub pdsData {
